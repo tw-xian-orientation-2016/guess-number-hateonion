f95dbe778b4900f6511f18577b8ba6aced4d4a19
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2016-03-04 11:17:38 +0800

    refactor:polish testCase

--- src/CompareNumber.java
+++ src/CompareNumber.java
@@ -1,15 +1,14 @@
 public class CompareNumber{
 
-    public String compare(String inputNumber, String correctNumber) {
-        int a = 0;
-        int b = 0;
-        for(int i = 0; i < correctNumber.length(); i++) {
-            if(inputNumber.charAt(i) == correctNumber.charAt(i)) {
+    public String compare(String input, String answer) {
+        int a = 0, b = 0;
+        for(int i = 0; i < answer.length(); i++) {
+            if(input.charAt(i) == answer.charAt(i)) {
                 a++;
-            } else if(correctNumber.indexOf(inputNumber.charAt(i)) != -1) {
+            } else if(answer.indexOf(input.charAt(i)) != -1) {
                 b++;
             }
         }
-        return String.format("%dA%dB",a,b);
+        return String.format("%dA%dB", a, b);
     }
 }
--- test/CompaNumberTest.java
+++ test/CompaNumberTest.java
@@ -7,30 +7,29 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class CompaNumberTest{
+
     private CompareNumber compareNumber;
+
     @Before
     public void init() {
         compareNumber = new CompareNumber();
     }
 
-
     @Test
-    public void should_return_4A0B() {
+    public void should_return_4A0B_when_all_match() {
         String result = compareNumber.compare("1234", "1234");
         assertThat(result, is("4A0B"));
     }
 
     @Test
-    public void should_return_0A4B() {
+    public void should_return_0A4B_when_all_include_but_not_match() {
         String result = compareNumber.compare("4321", "1234");
         assertThat(result, is("0A4B"));
     }
 
     @Test
-    public void should_return_0A0B() {
-        String result = compareNumber.compare("5678", "1234");
+    public void should_return_0A0B_when_neither_match_nor_include() {
+        String result = compareNumber.compare("7890", "1234");
         assertThat(result, is("0A0B"));
     }
-
-
 }
