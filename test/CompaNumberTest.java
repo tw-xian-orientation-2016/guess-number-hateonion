5ca0b536f7b84db6850c23bb66f4487ceb18346e
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2016-03-04 10:45:18 +0800

    feature finish this case

--- test/CompaNumberTest.java
+++ test/CompaNumberTest.java
@@ -1,24 +1,35 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.omg.CORBA.portable.Streamable;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class CompaNumberTest{
+    private CompareNumber compareNumber;
+    @Before
+    public void init() {
+        compareNumber = new CompareNumber();
+    }
+
 
     @Test
     public void should_return_4A0B() {
-        CompareNumber compareNumber = new CompareNumber();
         String result = compareNumber.compare("1234", "1234");
         assertThat(result, is("4A0B"));
     }
 
     @Test
     public void should_return_0A4B() {
-        CompareNumber compareNumber = new CompareNumber();
         String result = compareNumber.compare("4321", "1234");
         assertThat(result, is("0A4B"));
+    }
+
+    @Test
+    public void should_return_0A0B() {
+        String result = compareNumber.compare("5678", "1234");
+        assertThat(result, is("0A0B"));
     }
 
 
